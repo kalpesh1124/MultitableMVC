a4c0bfd514338068529cb5215d8d73a718ff85ca
kalpesh1124 <kalpeshinde170@gmail.com>
2023-09-14 00:29:35 +0530

    Initial commit

---
+++ MultitableMVC/Controllers/studController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultitableMVC.Models;
+
+namespace MultitableMVC.Controllers
+{
+    public class studController : Controller
+    {
+        CompanyDataEntities2 db = new CompanyDataEntities2();
+        public ActionResult create()
+        {
+            return View();
+        }
+        
+        public ActionResult show()
+        {
+            List<student> cn = db.students.ToList();
+            return View(cn);
+        }
+
+        public ActionResult Adddata()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Adddata(student s1) 
+        {
+            db.students.Add(s1);
+            db.SaveChanges();
+            return "Student Added Successfully !!!";
+        }
+    }
+}
---
+++ MultitableMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultitableMVC.MvcApplication" Language="C#" %>
